da91dd8ad87a34b9fcda205a0564b89fe382f528
Benjamin Kingsley (RIT Student) <bdk8938@rit.edu>
2026-03-30 12:31:33 -0400

    functional example 3

--- client/example3.jsx
+++ client/example3.jsx
@@ -1,11 +1,44 @@
 const React = require('react');
 const { createRoot } = require('react-dom/client');
+const { useState, useEffect } = React;
 
+const SongContainer = (props) => {
+    const [songs, setSongs] = useState(props.songs);
 
+    useEffect(() => {
+        const loadSongsFromServer = async () => {
+            const response = await fetch('/getSongs');
+            const data = await response.json();
+            setSongs(data);
+        }
+        loadSongsFromServer();
+    }, []);
+
+    if (songs.length === 0) {
+        return (
+            <div>
+                <h3>No Songs Yet!</h3>
+            </div>
+        );
+    }
+
+    const songList = songs.map(song => {
+        return (<div key={song.title}>
+            <h2>{song.artist} - <i>{song.title}</i></h2>
+        </div>);
+    });
+
+    return (
+        <div>
+            <h1>My Favorite Songs</h1>
+            {songList}
+        </div>
+    );
+}
 
 const init = () => {
     const root = createRoot(document.getElementById('app'));
-    root.render(<Song />);
+    root.render(<SongContainer songs={[]} />);
 }
 
 window.onload = init;
